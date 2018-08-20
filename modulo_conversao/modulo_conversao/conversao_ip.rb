34af8fdab49d6fc75395d4d16c814de50a8d14c2
José Luiz Corrêa Júnior <juninhopc@icloud.com>
2018-08-20 14:29:43 -0300

    varias adicoes de escopo de modulos

---
+++ modulo_conversao/modulo_conversao/conversao_ip.rb
@@ -0,0 +1,6 @@
+# Transforma lista de IPs e Links em somente IPs
+# José Luiz Corrêa Junior - juninhoojl
+# Escrito em Ruby
+# ----------------------------------------
+
+#Comando = resolveip www.google.com | cut -d" " -f6
